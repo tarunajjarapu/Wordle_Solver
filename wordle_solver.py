69829ff2878a76bf6db5dfbfd1ad4082ed78ddfb
tarunajjarapu <tarunajj@gmail.com>
2023-07-15 17:20:46 -0500

    Updated with comments

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,13 +1,26 @@
 # Name: Tarun Ajjarapu
 
-
 class WordleSolver:
-    
+
     def __init__(self):
         self.__all_words = []
         self.get_words()
 
     def get_guess(self, feedback):
+        """
+        Make a guess for the current round of Wordle.
+        :param feedback: A list of strings representing the guesses so far
+        and the feedback for those guesses in the current game of Wordle.
+        If feedback is empty then this is the first guess.
+        The order of the elements of feedback is [feedback_1, guess_1,
+        feedback_2, guess_2, ...]
+        All strings are length 7.
+        The feedback strings consist of G, O, and -.
+        G for GREEN, correct letter in correct spot.
+        O for letter in word but not in right spot.
+        - for letter not in word.
+        :return: A string that is in __all_words and is the next guess.
+        """
         current_words = self.__all_words
         feedback_count = 0
         guess_count = 1
@@ -63,6 +76,11 @@ class WordleSolver:
         return res.lower()
 
     def word_value(self, word):
+        """
+        This function determines the value of the word based on the weight
+        :param word: a set of a word to be used for comparison
+        :return: a double value for the total weight of the word
+        """
         word_freq = [8.5, 1.97, 4.62, 3.39, 11.16, 1.96, 2.48, 2.57, 7.54,
                      0.05, 1.29, 5.48, 2.78, 6.65, 7.16, 2.8, 0.04, 7.58, 5.73,
                      6.95, 3.63, 0.99, 1.73, 0.15, 1.9, 0.07]
@@ -70,15 +88,25 @@ class WordleSolver:
         for curr in word:
             total += word_freq[ord(curr) - ord('a')] * 7
         return total
-    
+
     def get_words(self):
-        
+        """ Read the words from the dictionary file and place them
+        in the __all_words instance variable.
+        We assume the  required files are in the current working directory
+        and is named all_words_5.txt. We also assume all words are
+        seven letters long, one word per line.
+        Returns a set of strings with all the words from the file.
+        """
         with open('all_words_7.txt', 'r') as data_file:
             all_lines = data_file.readlines()
             for line in all_lines:
                 self.__all_words.append(line.strip())
 
     def show_words(self):
+        """
+        Debugging method to check file was read in correctly.
+        :return: None
+        """
         print(len(self.__all_words))
         for word in self.__all_words:
             print(word)
