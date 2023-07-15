11982867c42201608bf8c8aad59947ca6a6120fb
tarunajjarapu <tarunajj@gmail.com>
2023-07-15 15:41:13 -0500

    Retrieving all words from text file

--- wordle_solver.py
+++ wordle_solver.py
@@ -6,3 +6,21 @@ class WordleSolver:
     def __init__(self):
         self.__all_words = []
         self.get_words()
+
+    def get_words(self):
+        
+        with open('all_words_7.txt', 'r') as data_file:
+            all_lines = data_file.readlines()
+            for line in all_lines:
+                self.__all_words.append(line.strip())
+
+    def show_words(self):
+        print(len(self.__all_words))
+        for word in self.__all_words:
+            print(word)
+
+def main():
+    wordle = WordleSolver()
+
+if __name__ == '__main__':
+    main()
